782ec0c33a60da76f1e518eab3c022a766d6e66a
jatin verma <jv.jatinverma.jv@gmail.com>
2021-11-27 00:08:25 +0530

    css files added and navbar html created

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ const app = express();
 const path = require('path');
 const port = process.env.port || 3000;
 
+// getting path
 const view_path = path.join(__dirname, "../template/views");
+const static_path = path.join(__dirname, "../public");
 
+// setting to hbs
 app.set('view engine', 'hbs');
 app.set('views', view_path);
+
+//for using static file
+app.use(express.static(static_path));
 
 app.get("/", (req, res)=>{
     res.render('index');
