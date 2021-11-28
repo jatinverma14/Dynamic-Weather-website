58042ab35c89c059d3ab28fda485edbbaebf5b8f
jatin verma <jv.jatinverma.jv@gmail.com>
2021-11-28 19:49:16 +0530

    some new pages created and use of partials started

--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const hbs = require('hbs');
 const port = process.env.port || 3000;
 
 // getting path
 const view_path = path.join(__dirname, "../template/views");
 const static_path = path.join(__dirname, "../public");
+const partial_path = path.join(__dirname, "../template/partials");
 
 // setting to hbs
 app.set('view engine', 'hbs');
 app.set('views', view_path);
+hbs.registerPartials(partial_path);
 
 //for using static file
 app.use(express.static(static_path));
 
 app.get("/", (req, res)=>{
     res.render('index');
+});
+
+app.get("/weather", (req, res)=>{
+    res.render('weather');
+});
+
+app.get("/quotes", (req, res)=>{
+    res.render('quotes');
+});
+
+app.get("/contact", (req, res)=>{
+    res.render('contact');
 });
 
 app.listen(port, ()=>{
